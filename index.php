b811208122fa1735b2d86af9453d913092d1c88a
pmpr.dev <pmpr.dev@gmail.com>
2025-09-29 20:04:32 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68aed62a61704             |
+    |             68dae62543f32             |
     |_______________________________________|
 */
  use Pmpr\Module\Rating\Rating; Rating::symcgieuakksimmu();
